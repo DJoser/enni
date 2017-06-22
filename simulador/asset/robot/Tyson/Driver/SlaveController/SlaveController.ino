3429f1049ad119656b0c8044319b3ce0eea43ac7
alfredopizana <pizana.alfredo@gmail.com>
2017-06-22 09:05:14 -0500

    Se Agrega Carpeta Driver Dentro del Simulador/Driver

---
+++ simulador/asset/robot/Tyson/Driver/SlaveController/SlaveController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SlaveController.ino
+ Created:	6/19/2017 4:57:46 PM
+ Author:	alfre
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
